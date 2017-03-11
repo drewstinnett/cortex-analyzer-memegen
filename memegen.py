bb52eedb8bc97ca38a06baeb1d84a67902b84407
Drew Stinnett <drew.stinnett@duke.edu>
2017-03-11 12:00:56 -0500

    Adding in a little documentation

--- memegen.py
+++ memegen.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import sys
 import requests
 from cortexutils.analyzer import Analyzer
 
 
-class URLCategoryAnalyzer(Analyzer):
+class MemeAnalyzer(Analyzer):
 
     def summary(self, raw):
-        return {'memes': raw['memes']}
+        """
+        This is the json that's returned in the report
+        """
+        return {
+            'meme_generators': raw['meme_generators'],
+            'meme_instances': raw['meme_instances']
+            }
 
     def run(self):
+        """
+        Run the analysis here
+        """
         Analyzer.run(self)
 
+        ## Only do 'meme' types right now
         if self.data_type == 'meme':
             try:
-				sys.stderr.write("Data: %s" % self.getData())
-				req = requests.get('http://version1.api.memegenerator.net/Generators_Search?q=%s&pageIndex=0&pageSize=5' % self.getData())
+
+                ## Just get some json, using the user input as the seach query
+				generators = requests.get('http://version1.api.memegenerator.net/Generators_Search?q=%s&pageIndex=0&pageSize=5' % self.getData()).json()
+				instances = requests.get('http://version1.api.memegenerator.net/Instances_Search?q=%s&pageIndex=0&pageSize=5' % self.getData()).json()
+
+                ## This gets put back to the summary report object
 				self.report({
-                    'memes': req.json()
+                    'meme_generators': generators,
+                    'meme_instances': instances,
                 })
+
             except ValueError as e:
                 self.unexpectedError(e)
         else:
             self.notSupported()
 
 if __name__ == '__main__':
-    URLCategoryAnalyzer().run()
+    MemeAnalyzer().run()
